69594bfda9d72c892c713f988c8b2a9063eabd00
Vivek Pannu <Vivek.Pannu@grapecity.com>
2022-03-03 16:49:58 +0530

    Created initial basic library

---
+++ CalculatorLibrary/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using CalculatorLibrary;
+
+class ConsoleCalculator
+{
+   static void Main()
+    {
+        Console.Write("Enter two numbers : \n\tFirst Number: ");
+        decimal firstNumber = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("\tSecond Number: ");
+        decimal secondNumber = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine($"You entered {firstNumber} and {secondNumber}");
+        Console.Write("Addition is : ");
+        Console.WriteLine(CalculatorOperations.addNumbers(firstNumber, secondNumber));
+
+        Console.Write("Subtraction is : ");
+        Console.WriteLine(CalculatorOperations.subtractNumbers(firstNumber, secondNumber));
+
+        Console.Write("Multiplication is : ");
+        Console.WriteLine(CalculatorOperations.multiplyNumbers(firstNumber, secondNumber));
+
+        Console.Write("Division is : ");
+        Console.WriteLine(CalculatorOperations.divideNumbers(firstNumber, secondNumber));
+    }
+}
---
+++ CalculatorLibrary/CalculatorLibrary/CalculatorOperations.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculatorLibrary
+{
+    public class CalculatorOperations
+    {
+        public static decimal addNumbers (decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+        public static decimal subtractNumbers(decimal number1, decimal number2)
+        {
+            return number1 - number2;
+        }
+        public static decimal multiplyNumbers(decimal number1, decimal number2)
+        {
+            return number1 * number2;
+        }
+        public static decimal divideNumbers(decimal number1, decimal number2)
+        {
+            return number1 / number2;
+        }
+    }
+  
+}
